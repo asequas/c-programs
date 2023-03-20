396aef060089270e74e401d332f1e3ad3e832e17
asequas <128362579+asequas@users.noreply.github.com>
2023-03-20 14:53:21 +0530

    Create Sorting list of numbers using arrays.c

---
+++ Sorting list of numbers using arrays.c
@@ -0,0 +1,22 @@
+//Sorting list of numbers using arrays
+#include <stdio.h>
+int main(){
+    int n,l1[30],i,j,a;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        scanf("%d",&l1[i-1]);
+    }
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if (l1[i]>l1[j]){
+                a=l1[i];
+                l1[i]=l1[j];
+                l1[j]=a;
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        printf("%d ",l1[i]);
+    }
+    return 0;
+}
